70ca5cbb260b0517c34b728fdbb5313863279e5d
Joni Hill <joni.hill@rocketmail.com>
2019-10-25 11:54:28 -0500

    added css to chores,movie,fav-photos

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Best Photos of the Week!';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
+  image2 = 'https://www.boletomachupicchu.com/gutblt/wp-content/uploads/2017/06/machu-picchu-amanecer.jpg';
   image3 = '';
 
   constructor() { }
